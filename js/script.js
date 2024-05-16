763f4ea4e1469b7a07312fdc5eb04e1dad0169f7
Edenilton <142240436+Edenilton2023@users.noreply.github.com>
2024-05-16 13:55:32 -0300

    aula 5
    finalizado a parte de  Responsividade

---
+++ js/script.js
@@ -0,0 +1,27 @@
+console.log('Jordan Shoes');
+
+const getProducts = async ()=>{
+    const response = await fetch(" js/products.json")
+    const data = await response.json()
+   
+    return data
+}
+
+
+const generateCard = async () => {
+    const products = await getProducts()
+ products.map(product=>{
+let card = document.createElement('div') 
+ card.classList.add('card__produtos')
+card.innerHTML = `   <figure>       <img src="images/${product.image}" alt="${product.product_name}">
+</figure>
+            <div class="card__produtos_detalhes">  <h4>${product.product_name}</h4>
+               <h5>${product.product_name}</h5>
+               </div>
+               <h6>R$ ${product.price}</h6>
+`;
+const listaProdutos = document.querySelector('.lista__produtos')
+listaProdutos.appendChild(card)
+ })
+}
+generateCard()
